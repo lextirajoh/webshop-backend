8b42a4604500fe6d481ce0be9de9c4beac2f12a4
Lex <alext@disroot.org>
2022-11-24 22:03:51 +0100

    Setup repo

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const products = require('./products');
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Bored Gamers API');
+});
+
+app.get('/products', (req, res) => {
+  res.send(products);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, console.log(`Server running on port ${port}`));
